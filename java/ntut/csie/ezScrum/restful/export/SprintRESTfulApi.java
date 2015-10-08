cc4c390b2b3fb3bdd25fab822e2234d2c68fd0ef
Jay-Liao <j99590314@gmail.com>
2015-10-08 14:54:37 +0800

    done first version sprint resource API

--- java/ntut/csie/ezScrum/restful/export/SprintRESTfulApi.java
+++ java/ntut/csie/ezScrum/restful/export/SprintRESTfulApi.java
@@ -1,5 +1,7 @@
 package ntut.csie.ezScrum.restful.export;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,18 +9,40 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("sprints")
+import org.codehaus.jettison.json.JSONException;
+
+import ntut.csie.ezScrum.iteration.core.ISprintPlanDesc;
+import ntut.csie.ezScrum.web.helper.SprintPlanHelper;
+import ntut.csie.ezScrum.web.mapper.ProjectMapper;
+import ntut.csie.ezScrum.web.support.export.SprintFluent;
+import ntut.csie.jcis.resource.core.IProject;
+
+@Path("projects/{projectName}/sprints")
 public class SprintRESTfulApi {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getList() {
-		return null;
+	public Response getList(@PathParam("projectName") String projectName) throws JSONException {
+		// Get project By name
+		IProject project = new ProjectMapper().getProjectByID(projectName);
+		// Get sprints
+		SprintPlanHelper sprintPlanHelper = new SprintPlanHelper(project);
+		List<ISprintPlanDesc> sprints = sprintPlanHelper.loadListPlans();
+		// Get Sprints JSONString
+		String entity = SprintFluent.toJSON(sprints).toString();
+		return Response.status(Response.Status.OK).entity(entity).build();
 	}
 
 	@GET
 	@Path("/{sprintId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@PathParam("sprintId") long sprintId) {
-		return null;
+	public Response get(@PathParam("projectName") String projectName, @PathParam("sprintId") long sprintId) throws JSONException {
+		// Get project By name
+		IProject project = new ProjectMapper().getProjectByID(projectName);
+		// Get sprint
+		SprintPlanHelper sprintPlanHelper = new SprintPlanHelper(project);
+		ISprintPlanDesc sprint = sprintPlanHelper.loadPlan(String.valueOf(sprintId));
+		// Get Sprint JSONString
+		String entity = SprintFluent.toJSON(sprint).toString();
+		return Response.status(Response.Status.OK).entity(entity).build();
 	}
 }
