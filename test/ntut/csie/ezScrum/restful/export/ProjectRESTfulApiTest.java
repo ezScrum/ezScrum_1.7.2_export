56845038791d04be3b3174aeef89a16069985dd0
ibmboy19 <ibmboy19@gmail.com>
2015-10-08 17:45:06 +0800

    Update Jersey to 2.22

--- test/ntut/csie/ezScrum/restful/export/ProjectRESTfulApiTest.java
+++ test/ntut/csie/ezScrum/restful/export/ProjectRESTfulApiTest.java
@@ -1,8 +1,23 @@
 package ntut.csie.ezScrum.restful.export;
 
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class ProjectRESTfulApiTest {
+public class ProjectRESTfulApiTest extends JerseyTest {
+
+	@Before
+	public void setUp() {
+		
+	}
+
+	@After
+	public void tearDown() {
+		
+	}
+
 	@Test
-	public void testGet() {}
+	public void testGet() {
+	}
 }
