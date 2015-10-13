fccbae2556a5b0d55788610b3fb901e57221b7bf
afa <c0960707618@gmail.com>
2015-10-13 16:35:27 +0800

    finish TaskRESTfulApi get, getList

--- java/ntut/csie/ezScrum/restful/export/TaskRESTfulApi.java
+++ java/ntut/csie/ezScrum/restful/export/TaskRESTfulApi.java
@@ -1,5 +1,9 @@
 package ntut.csie.ezScrum.restful.export;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,18 +11,52 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("tasks")
+import org.codehaus.jettison.json.JSONException;
+
+import ntut.csie.ezScrum.issue.core.IIssue;
+import ntut.csie.ezScrum.iteration.core.ScrumEnum;
+import ntut.csie.ezScrum.web.mapper.ProductBacklogMapper;
+import ntut.csie.ezScrum.web.mapper.ProjectMapper;
+import ntut.csie.ezScrum.web.support.export.JSONEncoder;
+import ntut.csie.ezScrum.web.support.export.TaskJSONEncoder;
+import ntut.csie.jcis.resource.core.IProject;
+
+
+@Path("projects/{projectName}/tasks")
 public class TaskRESTfulApi {
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getList() {
-		return null;
+	public Response getList(@PathParam("projectName") String projectName) throws JSONException {	
+		// Get project
+		IProject project = new ProjectMapper().getProjectByID(projectName);
+		// Create ProductbacklogMapper
+		ProductBacklogMapper productBacklogMapper = new ProductBacklogMapper(project, null);
+		// Get Tasks
+		IIssue[] tasksArray = productBacklogMapper.getIssues(ScrumEnum.TASK_ISSUE_TYPE);
+		// Add tasks to List
+		List<IIssue> tasks = new ArrayList<IIssue>();
+		tasks.addAll(Arrays.asList(tasksArray));
+
+		return Response.status(Response.Status.OK).entity(TaskJSONEncoder.toJSONArray(tasks)).build();
 	}
 
 	@GET
 	@Path("/{taskId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@PathParam("taskId") long taskId) {
-		return null;
+	public Response get(@PathParam("projectName") String projectName, @PathParam("taskId") long taskId) throws JSONException {
+		// Get project
+		IProject project = new ProjectMapper().getProjectByID(projectName);
+		// Create ProductbacklogMapper
+		ProductBacklogMapper productBacklogMapper = new ProductBacklogMapper(project, null);
+		// Get Tasks
+		IIssue[] tasksArray = productBacklogMapper.getIssues(ScrumEnum.TASK_ISSUE_TYPE);
+
+		for (IIssue task : tasksArray) {
+			if (task.getIssueID() == taskId) {
+				return Response.status(Response.Status.OK).entity(TaskJSONEncoder.toJSON(task).toString()).build();
+			}
+		}
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 }
