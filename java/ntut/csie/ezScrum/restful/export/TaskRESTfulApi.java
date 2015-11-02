5d821859cc3c347811446bcb194ba7f6ae68e802
ibmboy19 <ibmboy19@gmail.com>
2015-11-02 17:23:04 +0800

    Finish TaskRESTfulApi and UT

--- java/ntut/csie/ezScrum/restful/export/TaskRESTfulApi.java
+++ java/ntut/csie/ezScrum/restful/export/TaskRESTfulApi.java
@@ -1,6 +1,9 @@
 package ntut.csie.ezScrum.restful.export;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,7 +13,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import ntut.csie.ezScrum.issue.core.IIssue;
+import ntut.csie.ezScrum.issue.internal.IssueAttachFile;
 import ntut.csie.ezScrum.iteration.core.ISprintPlanDesc;
+import ntut.csie.ezScrum.web.control.ProductBacklogHelper;
 import ntut.csie.ezScrum.web.helper.SprintBacklogHelper;
 import ntut.csie.ezScrum.web.support.export.JSONEncoder;
 import ntut.csie.ezScrum.web.support.export.ResourceFinder;
@@ -38,6 +43,58 @@ public class TaskRESTfulApi {
 		// Get Tasks
 		IIssue[] tasks = sprintBacklogHelper.getTaskInStory(String.valueOf(storyId));
 		String entity = JSONEncoder.toTaskJSONArray(Arrays.asList(tasks)).toString();
+		return Response.status(Response.Status.OK).entity(entity).build();
+	}
+	
+	@GET
+	@Path("/{taskId}/attachfiles")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAttachFiles(@PathParam("projectName") String projectName,
+			                       @PathParam("sprintId") long sprintId,
+                                   @PathParam("storyId") long storyId,
+	        				       @PathParam("taskId") long taskId) {
+		ResourceFinder resourceFinder = new ResourceFinder();
+		IProject project = resourceFinder.findProject(projectName);
+		ISprintPlanDesc sprint = resourceFinder.findSprint(sprintId);
+		IIssue story = resourceFinder.findStory(storyId);
+		IIssue task = resourceFinder.findTask(taskId);
+
+		if (project == null || sprint == null ||
+		        story == null || task == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		ProductBacklogHelper productBacklogHelper = new ProductBacklogHelper(project, null);
+		List<File> sourceFiles = new ArrayList<File>();
+		List<IssueAttachFile> attachFiles = task.getAttachFile();
+		for (IssueAttachFile attachFile : attachFiles) {
+			String attachFileIdString = String.valueOf(attachFile.getAttachFileId());
+			File srouceFile = productBacklogHelper.getAttachFile(attachFileIdString);
+			sourceFiles.add(srouceFile);
+		}
+		String entity = JSONEncoder.toAttachFileJSONArray(attachFiles, sourceFiles).toString();
+		return Response.status(Response.Status.OK).entity(entity).build();
+	}
+
+	@GET
+	@Path("/{taskId}/partners")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getPartners(@PathParam("projectName") String projectName,
+	        					@PathParam("sprintId") long sprintId,
+	        					@PathParam("storyId") long storyId,
+	        					@PathParam("taskId") long taskId) {
+		ResourceFinder resourceFinder = new ResourceFinder();
+		IProject project = resourceFinder.findProject(projectName);
+		ISprintPlanDesc sprint = resourceFinder.findSprint(sprintId);
+		IIssue story = resourceFinder.findStory(storyId);
+		IIssue task = resourceFinder.findTask(taskId);
+
+		if (project == null || sprint == null ||
+		        story == null || task == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		@SuppressWarnings("deprecation")
+		String partnersString = task.getPartners();
+		String entity = JSONEncoder.toPartnerJSONArray(partnersString).toString();
 		return Response.status(Response.Status.OK).entity(entity).build();
 	}
 }
--- test/ntut/csie/ezScrum/restful/export/TaskRESTfulApiTest.java
+++ test/ntut/csie/ezScrum/restful/export/TaskRESTfulApiTest.java
@@ -2,6 +2,7 @@ package ntut.csie.ezScrum.restful.export;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Arrays;
 
@@ -10,6 +11,8 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
@@ -23,12 +26,17 @@ import ntut.csie.ezScrum.issue.core.IIssue;
 import ntut.csie.ezScrum.test.CreateData.AddStoryToSprint;
 import ntut.csie.ezScrum.test.CreateData.AddTaskToStory;
 import ntut.csie.ezScrum.test.CreateData.CopyProject;
+import ntut.csie.ezScrum.test.CreateData.CreateAccount;
 import ntut.csie.ezScrum.test.CreateData.CreateProductBacklog;
 import ntut.csie.ezScrum.test.CreateData.CreateProject;
 import ntut.csie.ezScrum.test.CreateData.CreateSprint;
 import ntut.csie.ezScrum.test.CreateData.InitialSQL;
 import ntut.csie.ezScrum.test.CreateData.ezScrumInfoConfig;
+import ntut.csie.ezScrum.web.databaseEnum.AccountEnum;
+import ntut.csie.ezScrum.web.databaseEnum.AttachFileEnum;
 import ntut.csie.ezScrum.web.helper.SprintBacklogHelper;
+import ntut.csie.ezScrum.web.mapper.SprintBacklogMapper;
+import ntut.csie.ezScrum.web.support.export.FileEncoder;
 import ntut.csie.ezScrum.web.support.export.JSONEncoder;
 import ntut.csie.jcis.resource.core.IProject;
 
@@ -78,7 +86,6 @@ public class TaskRESTfulApiTest extends JerseyTest {
 
 		// Create Client
 		mClient = ClientBuilder.newClient();
-		
 	}
 
 	@After
@@ -124,5 +131,113 @@ public class TaskRESTfulApiTest extends JerseyTest {
 								   .get();
 		assertEquals(JSONEncoder.toTaskJSONArray(Arrays.asList(tasks)).toString(), response.readEntity(String.class));
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testGetAttachFiles() throws JSONException {
+		// Test Data
+		String testFile = "./TestData/RoleBase.xml";
+		IProject project = mCP.getProjectList().get(0);
+		String sprintId = mCS.getSprintIDList().get(0);
+		IIssue story = mASTS.getIssueList().get(0);
+		IIssue task = mATTS.getTaskList().get(0);
+
+		// Upload Attach File
+		SprintBacklogMapper sprintBacklogMapper = new SprintBacklogMapper(project, null);
+		sprintBacklogMapper.addAttachFile(task.getIssueID(), testFile);
+
+		// Call '/projects/{projectName}/sprints/{sprintId}/stories/{storyId}/tasks/{taskId}/attachfiles' API
+		Response response = mClient.target(mBaseUri)
+		        .path("projects/" + project.getName() +
+		                "/sprints/" + sprintId +
+		                "/stories/" + story.getIssueID() +
+		                "/tasks/" + task.getIssueID() +
+		                "/attachfiles")
+		        .request()
+		        .get();
+
+		JSONArray jsonResponse = new JSONArray(response.readEntity(String.class));
+		String expectedXmlBinary = FileEncoder.toBase64BinaryString(new File(testFile));
+
+		// Assert
+		assertEquals(1, jsonResponse.length());
+		assertEquals("RoleBase.xml", jsonResponse.getJSONObject(0).getString(AttachFileEnum.NAME));
+		assertEquals("text/xml", jsonResponse.getJSONObject(0).getString(AttachFileEnum.CONTENT_TYPE));
+		assertEquals(expectedXmlBinary, jsonResponse.getJSONObject(0).getString(AttachFileEnum.BINARY));
+	}
+	
+	@Test
+	public void testGetAttachFiles_MultipleFiles() throws JSONException {
+		// Test Data
+		String testFile1 = "./TestData/RoleBase.xml";
+		String testFile2 = "./TestData/InitialData/ScrumRole.xml";
+		IProject project = mCP.getProjectList().get(0);
+		String sprintId = mCS.getSprintIDList().get(0);
+		IIssue story = mASTS.getIssueList().get(0);
+		IIssue task = mATTS.getTaskList().get(0);
+
+		// Upload Attach File
+		SprintBacklogMapper sprintBacklogMapper = new SprintBacklogMapper(project, null);
+		sprintBacklogMapper.addAttachFile(task.getIssueID(), testFile1);
+		sprintBacklogMapper.addAttachFile(task.getIssueID(), testFile2);
+
+		// Call '/projects/{projectName}/sprints/{sprintId}/stories/{storyId}/tasks/{taskId}/attachfiles' API
+		Response response = mClient.target(mBaseUri)
+		        .path("projects/" + project.getName() +
+		                "/sprints/" + sprintId +
+		                "/stories/" + story.getIssueID() +
+		                "/tasks/" + task.getIssueID() +
+		                "/attachfiles")
+		        .request()
+		        .get();
+
+		JSONArray jsonResponse = new JSONArray(response.readEntity(String.class));
+		String expectedXmlBinary1 = FileEncoder.toBase64BinaryString(new File(testFile1));
+		String expectedXmlBinary2 = FileEncoder.toBase64BinaryString(new File(testFile2));
+
+		// Assert
+		assertEquals(2, jsonResponse.length());
+		assertEquals("RoleBase.xml", jsonResponse.getJSONObject(0).getString(AttachFileEnum.NAME));
+		assertEquals("text/xml", jsonResponse.getJSONObject(0).getString(AttachFileEnum.CONTENT_TYPE));
+		assertEquals(expectedXmlBinary1, jsonResponse.getJSONObject(0).getString(AttachFileEnum.BINARY));
+		
+		assertEquals("ScrumRole.xml", jsonResponse.getJSONObject(1).getString(AttachFileEnum.NAME));
+		assertEquals("text/xml", jsonResponse.getJSONObject(1).getString(AttachFileEnum.CONTENT_TYPE));
+		assertEquals(expectedXmlBinary2, jsonResponse.getJSONObject(1).getString(AttachFileEnum.BINARY));
+	}
+
+	@Test
+	public void testGetPartners() throws JSONException {
+		// Test Data
+		IProject project = mCP.getProjectList().get(0);
+		String sprintId = mCS.getSprintIDList().get(0);
+		IIssue story = mASTS.getIssueList().get(0);
+		IIssue task = mATTS.getTaskList().get(0);
+				
+		// Create Account
+		CreateAccount createAccount = new CreateAccount(2);
+		createAccount.exe();
+
+		// Add Partners to Task
+		String partners = createAccount.getAccountList().get(0).getName() + ";" + createAccount.getAccountList().get(1).getName();
+		SprintBacklogHelper sprintBacklogHelper = new SprintBacklogHelper(project, null);
+		sprintBacklogHelper.checkOutTask(task.getIssueID(), task.getSummary(), task.getAssignto(), partners, "", null);
+		
+		// Call '/projects/{projectName}/sprints/{sprintId}/stories/{storyId}/tasks/{taskId}/partners' API
+		Response response = mClient.target(mBaseUri)
+		        .path("projects/" + project.getName() +
+		                "/sprints/" + sprintId +
+		                "/stories/" + story.getIssueID() +
+		                "/tasks/" + task.getIssueID() +
+		                "/partners")
+		        .request()
+		        .get();
+		
+		JSONArray jsonResponse = new JSONArray(response.readEntity(String.class));
+
+		// Assert
+		assertEquals(2, jsonResponse.length());
+		assertEquals(createAccount.getAccountList().get(0).getName(), jsonResponse.getJSONObject(0).getString(AccountEnum.USERNAME));
+		assertEquals(createAccount.getAccountList().get(1).getName(), jsonResponse.getJSONObject(1).getString(AccountEnum.USERNAME));
 	}
 }
