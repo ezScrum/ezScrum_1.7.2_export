4204dce558cbb4d5d84cc8811b009514df5d30eb
afa <c0960707618@gmail.com>
2015-12-25 16:59:29 +0800

    add attach file & FileEncoder test & fix FileEncoder

--- java/ntut/csie/ezScrum/restful/export/support/FileEncoder.java
+++ java/ntut/csie/ezScrum/restful/export/support/FileEncoder.java
@@ -2,8 +2,7 @@ package ntut.csie.ezScrum.restful.export.support;
 
 import java.io.File;
 import java.io.IOException;
-
-import org.aspectj.util.FileUtil;
+import java.nio.file.Files;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
@@ -13,7 +12,8 @@ public class FileEncoder {
 		String base64BinaryString = "";
 		byte[] byteArray;
 		try {
-			byteArray = FileUtil.readAsByteArray(sourceFile);
+			// Read bytes from File 
+			byteArray = Files.readAllBytes(sourceFile.toPath());
 			base64BinaryString = Base64.encode(byteArray);
 		} catch (IOException e) {
 			e.printStackTrace();
--- test/ntut/csie/ezScrum/restful/export/support/FileEncoderTest.java
+++ test/ntut/csie/ezScrum/restful/export/support/FileEncoderTest.java
@@ -6,18 +6,28 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.syndication.io.impl.Base64;
-
-import ntut.csie.ezScrum.restful.export.support.FileEncoder;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class FileEncoderTest {
 	private final String SOURCE_CONTENT = "TEST_TO_BASE64_BINARY";
 	private final String FILE_PATH = "./TestData/source.txt";
+	private final String TXT_FILE_PATH = "./TestData/attachFiles/txt測試.txt";
+	private final String RAR_FILE_PATH = "./TestData/attachFiles/rar測試.rar";
+	private final String DOCX_FILE_PATH = "./TestData/attachFiles/docx測試.docx";
+	private final String DOC_FILE_PATH = "./TestData/attachFiles/doc測試.doc";
+	private final String JPEG_FILE_PATH = "./TestData/attachFiles/jpeg測試.jpg";
+	private final String PDF_FILE_PATH = "./TestData/attachFiles/pdf測試.pdf";
+	private final String PNG_FILE_PATH = "./TestData/attachFiles/png測試.png";
+	private final String XLSX_FILE_PATH = "./TestData/attachFiles/xlsx測試.xlsx";
+	private final String XLS_FILE_PATH = "./TestData/attachFiles/xls測試.xls";
+	private final String XML_FILE_PATH = "./TestData/attachFiles/xml測試.xml";
 	private File mSourceFile;
 
 	@Before
@@ -40,8 +50,80 @@ public class FileEncoderTest {
 	}
 
 	@Test
-	public void testToBase64BinaryString() {
-		String expectedOutput = Base64.encode(SOURCE_CONTENT);
+	public void testToBase64BinaryString() throws UnsupportedEncodingException {
+		String expectedOutput = Base64.encode(SOURCE_CONTENT.getBytes("UTF-8"));
 		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(mSourceFile));
+	}
+	
+	@Test
+	public void testToBase64BinaryString_Encoding_Txt() throws IOException {
+		File file = new File(TXT_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	
+	@Test
+	public void testToBase64BinaryString_Encoding_Rar() throws IOException {
+		File file = new File(RAR_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Docx() throws IOException {
+		File file = new File(DOCX_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Doc() throws IOException {
+		File file = new File(DOC_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Jpeg() throws IOException {
+		File file = new File(JPEG_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Pdf() throws IOException {
+		File file = new File(PDF_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Png() throws IOException {
+		File file = new File(PNG_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Xlsx() throws IOException {
+		File file = new File(XLSX_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Xls() throws IOException {
+		File file = new File(XLS_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
+	}
+	@Test
+	public void testToBase64BinaryString_Encoding_Xml() throws IOException {
+		File file = new File(XML_FILE_PATH);
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		String expectedOutput = Base64.encode(bytes);
+		assertEquals(expectedOutput, FileEncoder.toBase64BinaryString(file));
 	}
 }
